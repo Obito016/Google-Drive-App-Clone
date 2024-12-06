4d7c0d2eed175fb85f18026e1acd17a97ac59959
Obito016 <amoghmalviyawork@gmail.com>
2024-12-07 03:05:05 +0530

    bkchodi

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Drive Clone",
+  title: "ye kya bakchodi h bhai ",
   description: "StoreIt - the only storage solution you need",
 };
 
